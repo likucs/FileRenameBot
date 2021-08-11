78feba3cbf239e378375865db52ffd529a763ad3
Liku cs <83144124+likucs@users.noreply.github.com>
2021-08-11 17:05:02 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -102,12 +102,12 @@ async def start_me(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Join Channel', url='https://t.me/mod_apk_premium_cs'),
+                    InlineKeyboardButton('Owner 👑', url='https://t.me/iAmLiKu1')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Join Chat Group', url='https://t.me/discuss_group_cs'),
+                    InlineKeyboardButton('Movies Group', url='https://t.me/moviesprovider0')
                 ]
             ]
         ),
